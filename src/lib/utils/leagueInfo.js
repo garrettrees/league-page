fa7cfb9709f8cd70fdaf97cb19f58a48b0b12d3f
Garrett Rees <39204429+garrettrees@users.noreply.github.com>
2025-02-12 11:57:55 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,4 +1,4 @@
-/*   STEP 1   */
+k/*   STEP 1   */
 export const leagueID = "1124852596587126784"; // your league ID
 export const leagueName = "Utah Valley Fantasy"; // your league name
 export const dues = 100; // (optional) used in template constitution page
